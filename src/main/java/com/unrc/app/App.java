9cc33d08c3032271fce06dedbec1a2991c9e0151
Ezequiel <ezedepetris@gmail.com>
2015-07-08 21:43:38 -0300

    :beer: :beers: :+1: PC only Plays

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -14,56 +14,66 @@ public class App{
   public static void main(String[] args) {
     externalStaticFileLocation("./media");
 
-	  get("/main", (request, response) -> {
-	    return new ModelAndView(null, "main.moustache");
-		},
-	    new MustacheTemplateEngine()
-	  );
+		// public static AtomProblem Variable.atoms;
+		// public static MinMaxEngine<AtomProblem,AtomState> Variable.engine;
+   //  before((request, response) -> {
+   //  	AtomProblem Variable.atoms;
+			// MinMaxEngine<AtomProblem,AtomState> Variable.engine;
+   //  });
 
-		post("/dificulty", (request, response) -> {
-      return new ModelAndView(null, "dificulty.moustache");
-		},
-      new MustacheTemplateEngine()
-	  );
 
-	  post("/player", (request, response) -> {
-	  	request.session(true);
-	  	Integer dificulty = Integer.parseInt(request.queryParams("dificulty"));
-	  	request.session().attribute("dificulty",dificulty);
-      return new ModelAndView(null, "player.moustache");
-		},
-      new MustacheTemplateEngine()
-	  );
+  get("/main", (request, response) -> {
+    return new ModelAndView(null, "main.moustache");
+	},
+    new MustacheTemplateEngine()
+  );
 
-	  post("/play", (request, response) -> {
-	   	Map<String, Object> attributes = new HashMap<>();
-	  	Integer dificulty = request.session().attribute("dificulty");
-	  	Integer player = Integer.parseInt(request.queryParams("player"));
-	  	AtomProblem atoms = new AtomProblem();
-	  	//===================================NEW CODE==========================================================
-	  	String position = request.session().attribute("position");
-	  	char[] positionArray = position.toCharArray();
-	  	int i;
-	  	int j;
-	  	if (position.length()==2){//first number is a row and second is a column
-	  		i = positionArray[0];
-	  		j = positionArray[1];
-	  		atoms = new AtomProblem (atoms.putAnAtom(atoms.initialState(),(i-1),(j-1),new Atom(player)));
-	  	}
-	  	else{//first pair of number is a row and the other number is a column
-	  		i = 10;
-	  		j = positionArray[3];
-	  		atoms = new AtomProblem (atoms.putAnAtom(atoms.initialState(),(i-1),(j-1),new Atom(player)));
-	  	}
-	  	//====================================================================================================
-	  	MinMaxEngine<AtomProblem,AtomState> engine = new MinMaxEngine<AtomProblem,AtomState>(atoms,dificulty);
+	post("/dificulty", (request, response) -> {
+  	return new ModelAndView(null, "dificulty.moustache");
+	},
+  	new MustacheTemplateEngine()
+  );
 
-	  	atoms = new AtomProblem(engine.computeSuccessor(atoms.initialState().clone()));
-	  	attributes.put("atom",atoms.initialState());
-      return new ModelAndView(attributes, "play.moustache");
-		},
-      new MustacheTemplateEngine()
-	  );
+  post("/player", (request, response) -> {
+  	request.session(true);
+  	Integer dificulty = Integer.parseInt(request.queryParams("dificulty"));
+  	request.session().attribute("dificulty",dificulty);
+ 	 return new ModelAndView(null, "player.moustache");
+	},
+  new MustacheTemplateEngine()
+  );
+
+  post("/playAux", (request, response) -> {
+   	Map<String, Object> attributes = new HashMap<>();
+  	Integer dificulty = request.session().attribute("dificulty");
+  	Integer player = Integer.parseInt(request.queryParams("player"));
+  	
+  	Variable.atoms  = new AtomProblem();
+  	Variable.engine = new MinMaxEngine<AtomProblem,AtomState>(Variable.atoms,dificulty);
+  	Variable.atoms = new AtomProblem(Variable.engine.computeSuccessor(Variable.atoms.initialState().clone()));
+  	attributes.put("atom",Variable.atoms.initialState());
+  	return new ModelAndView(attributes, "play.moustache");
+	},
+  	new MustacheTemplateEngine()
+  );
+
+
+	post("/play",(request,response) -> {
+		Map<String, Object> attributes = new HashMap<>();
+  	String row = request.queryParams("row");
+  	String column =request.queryParams("column");
+  	// AtomProblem Variable.atoms = (AtomProblem)request.queryParams("atom");
+  	request.session().removeAttribute("atom");
+  	Integer dificulty = request.session().attribute("dificulty");
+  	 Variable.engine = new MinMaxEngine<AtomProblem,AtomState>(Variable.atoms,dificulty);
+  	
+  	Variable.atoms = new AtomProblem(Variable.engine.computeSuccessor(Variable.atoms.initialState().clone()));
+  	attributes.put("atom",Variable.atoms.initialState());
+  	return new ModelAndView(attributes,"play.moustache");
+	},
+		new MustacheTemplateEngine()
+	);
+
 
   }
 }
